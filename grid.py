90be045b248d8d34f090343170b39e009a4f9766
ileocho <leo.innocenzi@ipsa.fr>
2022-12-26 15:25:52 +0100

    added final grid environment, raycast for wall-blocking aura

--- grid.py
+++ grid.py
@@ -41,6 +41,7 @@ class GridEnv():
             distances = []
             neighbors_index = []
 
+            # starting from wall cell, find all neighbors within 1 blocks distance from the wall
             for i in range(self.grid_size):
                 for j in range(self.grid_size):
                     distance = np.sqrt((x - i) ** 2 + (y - j) ** 2)
@@ -48,6 +49,7 @@ class GridEnv():
                         distances.append(distance)
                         neighbors_index.append([i, j])
 
+            # if the neighbor is not writable, then it is a wall aura
             for i in range(len(distances)):
                 if neighbors_index[i] not in self.non_writable:
                     if distances[i] <= np.sqrt(2):
@@ -59,7 +61,7 @@ class GridEnv():
     def place_humans(self, humans):
         for human in humans:
             self.state = (self.grid_size - human[0], human[1] - 1)
-            self.grid[self.state] = 1
+            self.grid[self.state] = 1.2
             self.humans_cells.append(self.state)
             self.non_writable.append(self.state)
             self.human_aura()
@@ -69,39 +71,82 @@ class GridEnv():
         distances = []
         neighbors_index = []
 
+        # starting from human cell, find all neighbors within 2 blocks distance from the human
         for i in range(self.grid_size):
             for j in range(self.grid_size):
                 distance = np.sqrt((x - i) ** 2 + (y - j) ** 2)
-
-                if distance <= 2*np.sqrt(2) and distance > 0:
+                if distance <= np.sqrt(2)*2 and distance > 0:
                     distances.append(distance)
                     neighbors_index.append([i, j])
 
-        # propagate the aura from the human to the neighbors cells
-        # when a wall is encountered, the aura stops propagating in that direction
+        # cast a ray from the human to the neighbors and check if there is a wall in between
+        # if there is a wall, then the neighbor is not visible to the human
         for i in range(len(distances)):
-            # if neighbor cell is a wall
-            # push the cell after the wall in the direction of the wall to the non_writable list
-
-            if neighbors_index[i] in self.wall_cells:
-                if x == neighbors_index[i][0]:
-                    if y < neighbors_index[i][1]:
-                        self.non_writable.append([neighbors_index[i][0], neighbors_index[i][1] + 1])
-                    else:
-                        self.non_writable.append([neighbors_index[i][0], neighbors_index[i][1] - 1])
-                elif y == neighbors_index[i][1]:
-                    if x < neighbors_index[i][0]:
-                        self.non_writable.append([neighbors_index[i][0] + 1, neighbors_index[i][1]])
-                    else:
-                        self.non_writable.append([neighbors_index[i][0] - 1, neighbors_index[i][1]])
-
             if neighbors_index[i] not in self.non_writable:
                 if distances[i] <= np.sqrt(2):
-                    self.grid[neighbors_index[i][0], neighbors_index[i][1]] += 0.6
-                elif distances[i] <= 2*np.sqrt(2):
-                    self.grid[neighbors_index[i][0], neighbors_index[i][1]] += 0.3
+                    self.grid[neighbors_index[i][0], neighbors_index[i][1]] = 0.6
+                    # self.non_writable.append(neighbors_index[i])
+                elif np.sqrt(2) < distances[i] <= np.sqrt(2)*2:
+                    if self.ray_casting(self.state, neighbors_index[i]) == True:
+                        # if the ray is not blocked, then the neighbor is visible to the human
+                        self.grid[neighbors_index[i][0], neighbors_index[i][1]] = 0.3
+                        # self.non_writable.append(neighbors_index[i])
                 else:
                     pass
+
+    def ray_casting(self, start, end):
+        wall_cells = self.wall_cells
+        x1, y1 = start[0], start[1]
+        x2, y2 = end[0], end[1]
+
+        # we take the absolute value of the difference between the two points
+        dx = x2 - x1
+        dy = y2 - y1
+        # we check if the line is steep or strait
+        is_steep = abs(dy) > abs(dx)
+
+        if is_steep:
+            # if the line is steep, we swap the x and y coordinates
+            x1, y1 = y1, x1
+            x2, y2 = y2, x2
+
+        # we check if the line is going from left to right
+        if x1 > x2:
+            # if it is not, we swap the start and end points
+            x1, x2 = x2, x1
+            y1, y2 = y2, y1
+
+        # we calculate the difference between the two points
+        dx = x2 - x1
+        dy = y2 - y1
+
+        # we calculate the error
+        error = int(dx / 2.0)
+
+        # we calculate the ystep (how much we are going to add to y every time)
+        ystep = 1 if y1 < y2 else -1
+
+        y = y1
+        # we iterate over the points of the line
+        for x in range(x1, x2 + 1):
+            # we check if the line is steep and if it is, we swap the x and y coordinates
+            coord = [y, x] if is_steep else [x, y]
+
+            if coord in wall_cells:
+                # if the coordinate is in the wall_cells list, then there is a wall in between
+                # we return False to indicate that the ray is blocked
+                return False
+            # we add the error to y
+            error -= abs(dy)
+            if error < 0:
+                # if the error is less than 0, we add the ystep to y
+                y += ystep
+                # we add the dx to the error
+                error += dx
+
+        # if we get to this point, it means that the ray is not blocked
+        return True
+
 
     def display(self, inverted=False):
         template = 'plotly_white'
